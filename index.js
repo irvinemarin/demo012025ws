07b4255e428217a64c42cfeb713c9e8ea18d8178
irvinemarin <marinc.irvin@gmail.com>
2025-05-03 16:46:20 -0500

    jsondemo

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get('/', (req, res) => {
     res.send('¡Hola desde tu Web Service en Render!');
 });
 
+// Endpoint con JSON array personalizado
+app.get('/users', (req, res) => {
+    const usuarios = [
+      {
+        id: 1,
+        nombre: 'Juan Pérez',
+        email: 'juan@example.com'
+      },
+      {
+        id: 2,
+        nombre: 'María Gómez',
+        email: 'maria@example.com'
+      }
+    ];
+  
+    res.json(usuarios); // Esta línea responde con el JSON array
+  });
+
 // Empieza el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
