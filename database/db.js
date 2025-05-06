4d12e58c1aa18c00212a3ee4be37fa060dde478c
irvinemarin <marinc.irvin@gmail.com>
2025-05-06 13:37:12 -0500

    modify rejectUnauthorized

--- database/db.js
+++ database/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false  // <-- permite conexiones SSL sin validar certificado
+  }
 });
 
 module.exports = pool;
